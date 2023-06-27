437785cbc2f3c76e83949a3534039e7108b007b0
Zain <nuttyb35@gmail.com>
2023-06-27 14:17:04 -0400

    Initial Commit

---
+++ finalproject/finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectApp.swift
+//  finalproject
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
